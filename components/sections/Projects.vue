9354bbb696b5ff29ada83c5cbaa138f56946edc9
Smart Vain <zlc.blue15210@gmail.com>
2022-08-16 08:37:57 +0900

    add: projects area frame

--- components/sections/Projects.vue
+++ components/sections/Projects.vue
@@ -1,9 +1,13 @@
 <script setup lang="ts">
+const detail = 'These are my temporary projects.'
 </script>
 
 <template>
   <v-row justify="center" align="center" name="projects-area" class="section-area projects-area">
-    
+    <v-col cols="12" align="center">
+      <h1 class="section-title section-title-top-area">PROJECTS</h1>
+      <p v-text="detail" class="section-description section-description-top-area mt-11" />
+    </v-col>
   </v-row>
 </template>
 
