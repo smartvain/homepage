d3e69be2ae2e8ec0408cb0aad891b8aecf85a5b8
Smart Vain <zlc.blue15210@gmail.com>
2022-08-15 15:22:43 +0900

    add: scss type

--- types/scss.d.ts
+++ types/scss.d.ts
@@ -2,6 +2,7 @@ declare module '@/assets/module.scss' {
   const content: {
     MAIN_COLOR_LIGHT: string;
     MAIN_COLOR_DARK: string;
+    FOOTER_BACKGROUND_COLOR: string;
   };
   export default content;
 }
