99a9380dde1af1a8ca33fbe572708c7ad51603ed
Smart Vain <zlc.blue15210@gmail.com>
2022-08-15 20:55:00 +0900

    add: third color darken

--- types/scss.d.ts
+++ types/scss.d.ts
@@ -2,8 +2,9 @@ declare module '@/assets/scss/module.scss' {
   const content: {
     MAIN_COLOR_LIGHT: string;
     MAIN_COLOR_DARK: string;
-    THIRD_COLOR: string;
     SECONDARY_COLOR: string;
+    THIRD_COLOR: string;
+    THIRD_COLOR_DARKEN: string;
   };
   export default content;
 }
