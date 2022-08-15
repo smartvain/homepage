558db0e04ed576266b48a4fe4626e2f65b328733
Smart Vain <zlc.blue15210@gmail.com>
2022-08-15 17:33:02 +0900

    refactor: const color variable

--- types/scss.d.ts
+++ types/scss.d.ts
@@ -2,7 +2,7 @@ declare module '@/assets/scss/module.scss' {
   const content: {
     MAIN_COLOR_LIGHT: string;
     MAIN_COLOR_DARK: string;
-    FOOTER_BACKGROUND_COLOR: string;
+    THIRD_COLOR: string;
     SECONDARY_COLOR: string;
   };
   export default content;
