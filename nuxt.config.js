9a64b15a50f89b28f1838ef2ec605a3ec80facd2
Smart Vain <zlc.blue15210@gmail.com>
2022-08-15 14:26:48 +0900

    add: google fonts link

--- nuxt.config.js
+++ nuxt.config.js
@@ -17,7 +17,12 @@ export default {
       { hid: 'description', name: 'description', content: '' },
       { name: 'format-detection', content: 'telephone=no' },
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
+      { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: 'anonymous' },
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Ubuntu&display=swap' }
+    ],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
