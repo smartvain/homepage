10018add7a672a41d8c01a37e8f1987b239dbb26
Smart Vain <zlc.blue15210@gmail.com>
2022-08-17 00:55:43 +0900

    add: image hover animation

--- components/sections/Projects.vue
+++ components/sections/Projects.vue
@@ -1,25 +1,39 @@
 <script setup lang="ts">
+interface projectType {
+  title: string;
+  url: NodeRequire;
+  desc: string;
+}
+const sectionTitle = 'PROJECTS'
 const detail = 'These are my temporary projects.'
-const projects = [
+const projects: projectType[] = [
   { title: 'sample1', url: require('@/assets/images/sample-project.jpg'), desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book." },
   { title: 'sample2', url: require('@/assets/images/sample-project.jpg'), desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book." },
   { title: 'sample3', url: require('@/assets/images/sample-project.jpg'), desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry." },
-  { title: 'sample4', url: require('@/assets/images/sample-project.jpg'), desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book." },
-  { title: 'sample5', url: require('@/assets/images/sample-project.jpg'), desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book." },
 ]
 </script>
 
 <template>
-  <v-row justify="start" align="center" name="projects-area" class="section-area projects-area">
+  <v-row justify="start" name="projects-area" class="section-area projects-area">
     <v-col cols="12" align="center">
-      <h1 class="section-title section-title-top-area">PROJECTS</h1>
+      <h1 v-text="sectionTitle" class="section-title section-title-top-area" />
       <p v-text="detail" class="section-description section-description-top-area mt-11" />
     </v-col>
-    <v-col v-for="project in projects" cols="4" align-self="start">
-      <v-card class="rounded-lg pa-5 overflow-y-auto" height="500" flat>
-        <v-img class="mx-auto rounded-lg" :src="project.url" max-width="100%" :aspect-ratio="16/9"></v-img>
+    <v-col v-for="project in projects" cols="4">
+      <v-card class="rounded-lg pa-5 overflow-hidden" height="300" flat>
+        <v-hover v-slot="{ hover }">
+          <v-img class="mx-auto rounded-lg" :src="project.url" max-width="100%" :aspect-ratio="16/9">
+            <v-expand-transition>
+              <div v-if="hover" class="transition-fast-in-fast-out gradient-reveal" />
+            </v-expand-transition>
+            <v-expand-transition>
+              <div v-if="hover" class="transition-fast-in-fast-out text-reveal d-flex text-subtitle-1 white--text px-3">
+                {{ project.desc }}
+              </div>
+            </v-expand-transition>
+          </v-img>
+        </v-hover>
         <v-card-title class="justify-center">{{ project.title }}</v-card-title>
-        <v-card-text class="text-subtitle-1">{{ project.desc }}</v-card-text>
       </v-card>
     </v-col>
   </v-row>
@@ -27,6 +41,25 @@ const projects = [
 
 <style scoped lang="scss">
 .projects-area {
-  min-height: 77rem;
+  min-height: 45rem;
+}
+
+.gradient-reveal {
+  position: absolute;
+  top: 0;
+  opacity: .5;
+  height: 100%;
+  width: 100%;
+  background: linear-gradient(#af4cbb, #4e376b);
+}
+
+.text-reveal {
+  position: absolute;
+  bottom: 0;
+  opacity: .9;
+  height: 100%;
+  width: 100%;
+  align-items: center;
+  justify-content: center;
 }
 </style>
