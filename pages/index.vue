a9c36f3fec2b747d4f4dd009b02296092d779954
Smart Vain <zlc.blue15210@gmail.com>
2022-08-17 10:03:40 +0900

    add: function that get length from top

--- pages/index.vue
+++ pages/index.vue
@@ -3,14 +3,35 @@ import TopSection from '@/components/sections/Top.vue'
 import SkillsSection from '@/components/sections/Skills.vue'
 import ProjectsSection from '@/components/sections/Projects.vue'
 import ContactSection from '@/components/sections/Contact.vue'
+import { ComponentPublicInstance, onMounted, ref } from 'vue'
+
+const skillsSection   = ref<ComponentPublicInstance>()
+const projectsSection = ref<ComponentPublicInstance>()
+const contactSection  = ref<ComponentPublicInstance>()
+const skillsTop   = ref<number>(0)
+const projectsTop = ref<number>(0)
+const contactTop  = ref<number>(0)
+
+onMounted(() => {
+  if (skillsSection.value) {
+    skillsTop.value = skillsSection.value.$el.getBoundingClientRect().top
+  }
+  if (projectsSection.value) {
+    projectsTop.value = projectsSection.value.$el.getBoundingClientRect().top
+  }
+  if (contactSection.value) {
+    contactTop.value = contactSection.value.$el.getBoundingClientRect().top
+  }
+})
+
 </script>
 
 <template>
   <v-container fluid style="width: 93%; max-width: 102rem;">
     <TopSection />
-    <SkillsSection />
-    <ProjectsSection class="section-top-crevice" />
-    <ContactSection class="section-top-crevice" />
+    <SkillsSection ref="skillsSection" />
+    <ProjectsSection ref="projectsSection" class="section-top-crevice" />
+    <ContactSection ref="contactSection" class="section-top-crevice" />
   </v-container>
 </template>
 
