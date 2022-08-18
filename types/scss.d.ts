03e466fd821c3aae2eb0cc7d278f8e9426da115d
Smart Vain <zlc.blue15210@gmail.com>
2022-08-19 01:12:43 +0900

    refactor: scss module type name

--- types/scss.d.ts
+++ types/scss.d.ts
@@ -1,10 +1,10 @@
 declare module '@/assets/scss/module.scss' {
-  export const colorValues: {
+  export const colorModule: {
     MAIN_COLOR_LIGHT:   string;
     MAIN_COLOR_DARK:    string;
     SECONDARY_COLOR:    string;
     THIRD_COLOR:        string;
     THIRD_COLOR_DARKEN: string;
   };
-  export default colorValues;
+  export default colorModule;
 }
