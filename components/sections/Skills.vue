648e40f7274fd76571face5dd52e40e52be0fc53
Smart Vain <zlc.blue15210@gmail.com>
2022-08-19 01:11:12 +0900

    refactor: cut out code to SkillsContents component

--- components/sections/Skills.vue
+++ components/sections/Skills.vue
@@ -1,39 +1,8 @@
 <script setup lang="ts">
-import { computed } from 'vue'
-import { useContext } from '@nuxtjs/composition-api'
-import { SkillTypes } from '~/types/common';
-import colorModule from '@/assets/scss/module.scss'
-
-const { $vuetify } = useContext()
+import SkillsContents from './SkillsContents.vue'
 
 const sectionTitle  = 'SKILLS'
 const sectionDetail = 'Skills I can handle are listed here.'
-
-const skills: SkillTypes = {
-  frontend: [
-    { skillName: 'JavaScript', desc: 'Experienced',  year: 2 },
-    { skillName: 'TypeScript', desc: 'Intermediate', year: 1 },
-    { skillName: 'Vue.js',     desc: 'Experience',   year: 2 },
-    { skillName: 'Nuxt.js',    desc: 'Experience',   year: 2 },
-    { skillName: 'Vuetify',    desc: 'Experience',   year: 2 },
-    { skillName: 'CSS / SCSS', desc: 'Experienced',  year: 2 },
-    { skillName: 'HTML',       desc: 'Experienced',  year: 2 },
-    { skillName: 'Bootstrap',  desc: 'intermediate', year: 1 },
-  ],
-  backend: [
-    { skillName: 'PHP',     desc: 'Experience',   year: 3 },
-    { skillName: 'Laravel', desc: 'Experience',   year: 2 },
-    { skillName: 'MySQL',   desc: 'Experience',   year: 2 },
-    { skillName: 'Docker',  desc: 'Experience',   year: 2 },
-    { skillName: 'Node.js', desc: 'Intermediate', year: 1 },
-  ]
-}
-
-const cardBackGroundTheme = computed(() => {
-  return $vuetify.theme.dark
-    ? colorModule.THIRD_COLOR_DARKEN
-    : '#fff'
-})
 </script>
 
 <template>
@@ -47,23 +16,7 @@ const cardBackGroundTheme = computed(() => {
     <v-col cols="6" align="center">
       <h1 v-text="sectionTitle" class="section-title section-title-top-area" />
       <p v-text="sectionDetail" class="section-description section-description-top-area" />
-      <v-card height="500" width="1000" class="rounded-xl pa-7" :color="cardBackGroundTheme" flat>
-        <v-card-title class="justify-center font-weight-bold pa-0">Frontend Development</v-card-title>
-        <v-row class="overflow-y-auto mt-3" style="height: 90%">
-          <v-col v-for="(item, index) of skills.frontend" :key="`${index}${item.skillName}`" cols="6">
-            <v-card-title  class="ml-3">
-              <v-icon class="text-h5 mr-3">mdi-check-decagram</v-icon>
-              <span class="mr-5 text-h6">{{ item.skillName }}</span>
-              <span class="text-subtitle-2 grey--text">{{ item.desc }}</span>
-            </v-card-title>
-            <v-card-text>
-              <div class="progBar">
-                <div class="bar"></div>
-              </div>
-            </v-card-text>
-          </v-col>
-        </v-row>
-      </v-card>
+      <SkillsContents />
     </v-col>
   </v-row>
 </template>
@@ -84,7 +37,6 @@ const cardBackGroundTheme = computed(() => {
 
 // Float animation with rotation
 @keyframes float {
-
   0%,
   100% {
     transform: translateY(0) rotate(0deg);
@@ -92,33 +44,6 @@ const cardBackGroundTheme = computed(() => {
 
   50% {
     transform: translateY(-20px) rotate(-5deg);
-  }
-}
-
-.progBar {
-  width: 100%;
-  height: 1rem;
-  background-color: #ececec;
-  position: relative;
-  border-radius:.6rem;
-  overflow: hidden;
-}
-
-.progBar .bar {
-  position: absolute;
-  height: 100%;
-  background-color: #FFCA28;
-  animation: progress 4s normal;
-  animation-fill-mode: forwards;
-}
-
-@keyframes progress {
-  0% {
-    width: 0
-  }
-
-  100% {
-    width: 100%
   }
 }
 </style>
