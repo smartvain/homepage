de17093a0bce8b3b290972691d2e379d3e145b2a
Smart Vain <zlc.blue15210@gmail.com>
2022-08-18 20:57:59 +0900

    add: store variables

---
+++ store/index.ts
@@ -0,0 +1,15 @@
+import { InjectionKey, reactive } from "vue"
+
+export const topLengthsSet = (() => {
+  const topPlus = 20
+  
+  const topLengths = reactive({ skills: 0, projects: 0, contact: 0 })
+  
+  const setTopLengths = (areaName: keyof typeof topLengths, length: number): void => {
+    topLengths[areaName] = length + topPlus
+  }
+
+  return { topLengths, setTopLengths }
+})()
+
+export const topLengthsSetKey: InjectionKey<typeof topLengthsSet> = Symbol()
