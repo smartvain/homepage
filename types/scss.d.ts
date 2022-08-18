73bc9263dcd6bcab1bd26d3305e2d8c3f05f06e1
Smart Vain <zlc.blue15210@gmail.com>
2022-08-18 21:04:48 +0900

    refactor: scss.d.ts

--- types/scss.d.ts
+++ types/scss.d.ts
@@ -1,10 +1,10 @@
 declare module '@/assets/scss/module.scss' {
-  const content: {
-    MAIN_COLOR_LIGHT: string;
-    MAIN_COLOR_DARK: string;
-    SECONDARY_COLOR: string;
-    THIRD_COLOR: string;
+  export const colorValues: {
+    MAIN_COLOR_LIGHT:   string;
+    MAIN_COLOR_DARK:    string;
+    SECONDARY_COLOR:    string;
+    THIRD_COLOR:        string;
     THIRD_COLOR_DARKEN: string;
   };
-  export default content;
+  export default colorValues;
 }
