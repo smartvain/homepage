e2fab65a368bba804a29a95d8a9e95102cb5498c
Smart Vain <zlc.blue15210@gmail.com>
2022-08-18 21:02:58 +0900

    refactor & adjust: use provide()

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,27 +1,26 @@
 <script setup lang="ts">
-import { computed } from 'vue'
-import { useContext } from '@nuxtjs/composition-api'
 import colorModule from '@/assets/scss/module.scss'
-const { $vuetify } = useContext()
+import { useContext } from '@nuxtjs/composition-api';
+import { computed, provide } from 'vue';
+import { topLengthsSet, topLengthsSetKey } from '~/store';
+import { HeaderButtonType } from '~/types/common';
 
-// data
-const title         = 'Ryuichi Amejima'
+provide(topLengthsSetKey, topLengthsSet)
+
+const { $vuetify }   = useContext()
+const { topLengths } = topLengthsSet
+
+const logoTitle     = 'Ryuichi Amejima'
 const footerMessage = 'Ryuichi Amejima. All Rights Reserved.'
 const githubUrl     = 'https://github.com/smartvain'
-const addTop        = 20
-const topLengths = {
-  skillsTop:   818,
-  projectsTop: 1664,
-  contactTop:  2341,
-}
-const headerButtons = [
-  { text: 'Skills',   handleClick() { $vuetify.goTo(topLengths.skillsTop + addTop) } },
-  { text: 'Projects', handleClick() { $vuetify.goTo(topLengths.projectsTop + addTop) } },
-  { text: 'Contact',  handleClick() { $vuetify.goTo(topLengths.contactTop + addTop) } },
+
+const headerButtons :HeaderButtonType[] = [
+  { text: 'Skills',   handleClick() { $vuetify.goTo(topLengths.skills) } },
+  { text: 'Projects', handleClick() { $vuetify.goTo(topLengths.projects) } },
+  { text: 'Contact',  handleClick() { $vuetify.goTo(topLengths.contact) } },
   { text: 'Source',   handleClick() { window.open(githubUrl, '_blank') }, isIcon: true },
 ]
 
-// dark mode computed properties
 const backgroundTheme = computed(() => {
   return $vuetify.theme.dark
     ? colorModule.MAIN_COLOR_DARK
@@ -50,14 +49,14 @@ const githubLogoTheme = computed(() => {
     <v-app-bar color="transparent" flat fixed app>
       <v-btn plain @click="$vuetify.goTo(0)">
         <v-icon class="mr-2">mdi-space-invaders</v-icon>
-        <v-app-bar-title class="font-weight-bold" style="font-size: 1.6rem" v-text="title" />
+        <v-app-bar-title class="font-weight-bold" style="font-size: 1.6rem" v-text="logoTitle" />
       </v-btn>
       <v-spacer />
       <v-btn v-for="(item, index) in headerButtons" :key="index" class="mr-10 rounded-lg" @click="item.handleClick" outlined>
         <v-img v-if="item.isIcon" class="mr-2" max-width="1.5rem" :src="githubLogoTheme" />
         {{ item.text }}
       </v-btn>
-      <v-btn icon @click.stop="$vuetify.theme.dark = !$vuetify.theme.dark">
+      <v-btn class="mr-0" icon @click.stop="$vuetify.theme.dark = !$vuetify.theme.dark">
         <v-icon>{{ weatherIconTheme }}</v-icon>
       </v-btn>
     </v-app-bar>
