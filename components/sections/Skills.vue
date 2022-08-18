63c81c12d1d15a0ee66eb987ed17dd2e55ad4742
Smart Vain <zlc.blue15210@gmail.com>
2022-08-18 21:00:56 +0900

    refactor & add: implement skills card ui

--- components/sections/Skills.vue
+++ components/sections/Skills.vue
@@ -1,11 +1,32 @@
 <script setup lang="ts">
 import { computed } from 'vue'
 import { useContext } from '@nuxtjs/composition-api'
+import { SkillTypes } from '~/types/common';
 import colorModule from '@/assets/scss/module.scss'
+
 const { $vuetify } = useContext()
 
-const sectionTitle = 'SKILLS'
-const detail = 'Skills I can handle are listed here.'
+const sectionTitle  = 'SKILLS'
+const sectionDetail = 'Skills I can handle are listed here.'
+const skills: SkillTypes = {
+  frontend: {
+    'JavaScript': { year: 2, desc: 'Experienced' },
+    'TypeScript': { year: 1, desc: 'Intermediate' },
+    'Vue.js':     { year: 2, desc: 'Experience' },
+    'Nuxt.js':    { year: 2, desc: 'Experience' },
+    'Vuetify':    { year: 2, desc: 'Experience' },
+    'CSS / SCSS': { year: 2, desc: 'Experienced' },
+    'HTML':       { year: 2, desc: 'Experienced' },
+    'Bootstrap':  { year: 1, desc: 'intermediate' },
+  },
+  backend: {
+    'PHP':     { year: 3, desc: 'Experience' },
+    'Laravel': { year: 2, desc: 'Experience' },
+    'MySQL':   { year: 2, desc: 'Experience' },
+    'Docker':  { year: 2, desc: 'Experience' },
+    'Node.js': { year: 1, desc: 'Intermediate' },
+  }
+}
 
 const cardBackGroundTheme = computed(() => {
   return $vuetify.theme.dark
@@ -24,9 +45,23 @@ const cardBackGroundTheme = computed(() => {
     </v-col>
     <v-col cols="6" align="center">
       <h1 v-text="sectionTitle" class="section-title section-title-top-area" />
-      <p v-text="detail" class="section-description section-description-top-area mt-11" />
-      <v-card height="500" width="1000" class="rounded-xl" :color="cardBackGroundTheme" flat>
-
+      <p v-text="sectionDetail" class="section-description section-description-top-area" />
+      <v-card height="500" width="1000" class="rounded-xl pa-7" :color="cardBackGroundTheme" flat>
+        <v-card-title class="justify-center font-weight-bold pa-0">Frontend Development</v-card-title>
+        <v-row class="overflow-y-auto mt-3" style="height: 90%">
+          <v-col v-for="(item, skillName) in skills.frontend" cols="6">
+            <v-card-title  class="ml-3">
+              <v-icon class="text-h5 mr-3">mdi-check-decagram</v-icon>
+              <span class="mr-5 text-h6">{{ skillName }}</span>
+              <span class="text-subtitle-2 grey--text">{{ item.desc }}</span>
+            </v-card-title>
+            <v-card-text>
+              <div class="progBar">
+                <div class="bar"></div>
+              </div>
+            </v-card-text>
+          </v-col>
+        </v-row>
       </v-card>
     </v-col>
   </v-row>
@@ -48,11 +83,41 @@ const cardBackGroundTheme = computed(() => {
 
 // Float animation with rotation
 @keyframes float {
-  0%, 100% {
-    transform:translateY(0) rotate(0deg);
+
+  0%,
+  100% {
+    transform: translateY(0) rotate(0deg);
   }
+
   50% {
-    transform:translateY(-20px) rotate(-5deg);
+    transform: translateY(-20px) rotate(-5deg);
+  }
+}
+
+.progBar {
+  width: 100%;
+  height: 1rem;
+  background-color: #ececec;
+  position: relative;
+  border-radius:.6rem;
+  overflow: hidden;
+}
+
+.progBar .bar {
+  position: absolute;
+  height: 100%;
+  background-color: #FFCA28;
+  animation: progress 4s normal;
+  animation-fill-mode: forwards;
+}
+
+@keyframes progress {
+  0% {
+    width: 0
+  }
+
+  100% {
+    width: 100%
   }
 }
 </style>
