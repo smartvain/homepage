bbdbb9071fe15cd97a0b5fe04db18624f4d4ef1b
Smart Vain <zlc.blue15210@gmail.com>
2022-08-20 18:18:32 +0900

    add: customize component appear animation

--- components/ComponentAnimation.vue
+++ components/ComponentAnimation.vue
@@ -6,10 +6,13 @@
 
 <style scoped lang="scss">
 .fade-enter-active, .fade-leave-active {
-  transition: opacity .5s;
+  transition: transform 1s, opacity .6s;
+  opacity: 1;
+  transform: translateY(0);
   animation-delay: 2s;
 }
 .fade-enter, .fade-leave-to {
   opacity: 0;
+  transform: translateY(2rem);
 }
 </style>
