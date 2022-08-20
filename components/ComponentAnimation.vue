db28d4d5d2200e1f1c1fff18c5f73c1c180a48d7
Smart Vain <zlc.blue15210@gmail.com>
2022-08-20 15:07:54 +0900

    create: ComponentAnimation component

---
+++ components/ComponentAnimation.vue
@@ -0,0 +1,15 @@
+<template>
+  <transition name="fade">
+    <slot></slot>
+  </transition>
+</template>
+
+<style scoped lang="scss">
+.fade-enter-active, .fade-leave-active {
+  transition: opacity .5s;
+  animation-delay: 2s;
+}
+.fade-enter, .fade-leave-to {
+  opacity: 0;
+}
+</style>
