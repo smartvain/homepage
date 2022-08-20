8e45d90155576b80e04050ae9e589b3a03bc35c2
Smart Vain <zlc.blue15210@gmail.com>
2022-08-20 21:43:04 +0900

    add: sample project content

--- components/sections/Projects.vue
+++ components/sections/Projects.vue
@@ -18,6 +18,9 @@ const projects: ProjectType[] = [
   { title: 'My sample project1', url: require('@/assets/images/sample-project.png'), desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book." },
   { title: 'My sample project2', url: require('@/assets/images/sample-project.png'), desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book." },
   { title: 'My sample project3', url: require('@/assets/images/sample-project.png'), desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry." },
+  { title: 'My sample project1', url: require('@/assets/images/sample-project.png'), desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book." },
+  { title: 'My sample project2', url: require('@/assets/images/sample-project.png'), desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book." },
+  { title: 'My sample project3', url: require('@/assets/images/sample-project.png'), desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry." },
 ]
 
 const cardBackGroundTheme = computed(() => {
