89145cf79876cec521a4f13a5b00e37e9d96c057
Smart Vain <zlc.blue15210@gmail.com>
2022-08-23 16:39:29 +0900

    change: text size

--- components/items/ContactButton.vue
+++ components/items/ContactButton.vue
@@ -1,12 +1,16 @@
 <script setup lang="ts">
 import colorModule from '@/assets/scss/module.scss';
 import { inject, useContext } from '@nuxtjs/composition-api';
-import { topLengthsSetKey } from '~/store';
+import { screenWidthSetKey, topLengthsSetKey } from '~/store';
 
 const { $vuetify } = useContext()
-const topLengthsSet = inject(topLengthsSetKey)
-if (!topLengthsSet) throw new Error('topLengthsSet is undefined')
+const topLengthsSet  = inject(topLengthsSetKey)
+const screenWidthSet = inject(screenWidthSetKey)
+if (!topLengthsSet)  throw new Error('topLengthsSet is undefined')
+if (!screenWidthSet) throw new Error('screenWidthSet is undefined')
+const { isSmMedia }  = screenWidthSet
 const { topLengths } = topLengthsSet
+
 
 const handleContactButton = (): void => {
   $vuetify.goTo(topLengths.contact)
@@ -16,7 +20,7 @@ const handleContactButton = (): void => {
 <template>
   <v-btn
     class="mt-7 font-weight-bold white--text rounded-lg pa-5"
-    style="font-size: 1.5rem"
+    :class="!isSmMedia ? 'text-subtitle-1' : 'text-h5'"
     :color="colorModule.THIRD_COLOR"
     @click="handleContactButton"
   >Contact
