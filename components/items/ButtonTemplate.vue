d677924224e1553d62013a8d63b2375521d6fe59
Smart Vain <zlc.blue15210@gmail.com>
2022-08-25 16:57:46 +0900

    add: ButtonTemplate component

---
+++ components/items/ButtonTemplate.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import colorModule from '@/assets/scss/module.scss';
+import { inject } from '@nuxtjs/composition-api';
+import { screenWidthSetKey } from '~/store';
+
+const screenWidthSet = inject(screenWidthSetKey)
+if (!screenWidthSet) throw new Error('screenWidthSet is undefined')
+const { isSmMedia }  = screenWidthSet
+
+defineProps<{ clickEvent: Function }>()
+</script>
+
+<template>
+  <v-btn
+    class="font-weight-bold white--text rounded-lg pa-5"
+    :class="!isSmMedia ? 'text-subtitle-1' : 'text-h5'"
+    :color="colorModule.THIRD_COLOR"
+    @click="clickEvent"
+  ><slot />
+  </v-btn>
+</template>
