e9af85440ed527f760e05c2240ed6d424201dd29
Smart Vain <zlc.blue15210@gmail.com>
2022-08-28 16:03:18 +0900

    remove: servermiddleware

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,7 +3,6 @@ import colors from 'vuetify/es5/util/colors'
 export default {
   // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
   ssr: false,
-  serverMiddleware: ["~/api/"],
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: 'Home page',
